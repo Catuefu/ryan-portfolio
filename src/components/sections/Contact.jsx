5a7abed862b1e186054e36da6f3903332cc6fa36
ctv <umbhilryan@gmail.com>
2025-05-21 21:29:42 +0700

    fixed contact

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -32,7 +32,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="w-full sm:w-full md:w-[600px] px-4 md:px-10 mx-auto">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-cyan-400 to-purple-600 bg-clip-text text-center text-transparent">
             {" "}
             Get In Touch{" "}
